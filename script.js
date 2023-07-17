18f7ef2f097b921cc48e1056e2f28d2128dda1e8
daniel caballero <caballero.garay.daniel@gmail.com>
2023-07-17 00:32:56 -0300

    Se termina con la tabla dinamica y se agrega la funcion filtrar por nombre

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 
-// function ingresar() {
+
 let alumnos = [];
+//let alumno = {}; // Declarar alumno como variable global
+
 class nuevoAlumno {
-    constructor(nombre, apaellido, documento, nacimiento, numeroSocio) {
+    constructor(nombre, apellido, documento, nacimiento, numeroSocio) {
         this.nombre = nombre;
-        this.apellido = apaellido;
+        this.apellido = apellido;
         this.documento = documento;
         this.fechaNacimiento = nacimiento;
         this.numeroSocio = numeroSocio;
     }
-    //     toString(elemento){
-    //     return '${this.elemento}
-    //     // Apellido: ${this.apellido}\n 
-    //     // DNI: ${this.documento}\n 
-    //     // Nacimiento: ${this.fechaNacimiento}\n
-    //     // Keypass ${this.numeroSocio}\n`;
-    // }
 }
+
 function agregarAlumnos() {
     let nombre = prompt("Ingrese su nombre");
     let apellido = prompt("Ingrese su apellido");
-    let documento = prompt("Ingrese su dni");
+    let documento = prompt("Ingrese su DNI");
     let fechaNacimiento = prompt("Ingrese su fecha de nacimiento");
-    let numeroSocio = prompt("Ingese 3 numeros para su keypass");
+    let numeroSocio = prompt("Ingrese 3 números para su keypass");
 
     let alumno = new nuevoAlumno(nombre, apellido, documento, fechaNacimiento, numeroSocio);
+    return alumno;
+}
+
+function ingresar() {
+    let tabla = document.getElementById('filas');
+    tabla.innerHTML = '';
+    let alumno = agregarAlumnos();
     alumnos.push(alumno);
-    let filaActual = document.getElementById('filas').insertRow();
-    for (const valor in alumno) {
-        let celda1 = filaActual.insertCell()
-        celda1.innerHTML = alumno[valor];
+    //Generara la tabla con alumnos inscriptos
+    for (const item of alumnos) {
+        console.log("estoy adentro");
+        let filaActual = document.getElementById('filas').insertRow();
+        for (const valor in alumno) {
+            let celda1 = filaActual.insertCell();
+            celda1.innerHTML = item[valor];
+        }
+    }
+}
+function filtrarAlumnos() {
+    let filtro = document.getElementById('filtro-nombre').value;
+
+    let alumnosFiltrados = alumnos.filter((alumno) => {
+        return alumno.nombre.toLowerCase().includes(filtro.toLowerCase());
+    });
+
+    // Vaciar la tabla
+    let tabla = document.getElementById('filas');
+    tabla.innerHTML = '';
+
+    // Generar las filas solo para los alumnos filtrados
+    for (const item of alumnosFiltrados) {
+        let filaActual = tabla.insertRow();
+        for (const valor in item) {
+            let celda = filaActual.insertCell();
+            celda.innerHTML = item[valor];
+        }
     }
 }
